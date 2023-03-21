7be3b1bfcf55ac6f35f561ad703df52ba2d6fc38
GersonEC <gersonenriquez94@gmail.com>
2023-03-21 14:39:40 +0100

    Add save to bookshelf method

--- routes/bookshelf.js
+++ routes/bookshelf.js
@@ -2,10 +2,43 @@ export default async function bookshelf(fastify, opts) {
   const { httpErrors, jwt } = fastify;
 
   fastify.route({
+    method: 'POST',
+    path: '/bookshelf',
+    handler: onSaveToBookshelf,
+  });
+
+  fastify.route({
     method: 'GET',
     path: '/bookshelf/:userId',
     handler: onUserBookshelf,
   });
+
+  async function onSaveToBookshelf(req, reply) {
+    return true;
+    // try {
+    //   const { userId, book } = JSON.parse(
+    //     request.body as string
+    //   ) as SaveToBookshelfBody;
+    //   const data = book;
+    //   data.userIDs = [userId];
+    //   const persistedBook: Prisma.BookCreateInput = await prisma.book.create({
+    //     data,
+    //   });
+    //   //Update the user with the book id associated
+    //   const updateUser = await prisma.user.update({
+    //     where: {
+    //       id: userId,
+    //     },
+    //     data: {
+    //       bookIDs: persistedBook.id,
+    //     },
+    //   });
+    //   return persistedBook;
+    // } catch (error) {
+    //   console.log('##### ERROR ####: ', error);
+    //   throw new Error(error as string);
+    // }
+  }
 
   async function onUserBookshelf(req, reply) {
     const { userId } = request.params;
