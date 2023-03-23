f935fc8025df61fd3e0c4147ac1a68f732ddeb59
GersonEC <gersonenriquez94@gmail.com>
2023-03-23 11:21:41 +0100

    Implemented onSaveToBookshelf with sqlite

--- routes/bookshelf.js
+++ routes/bookshelf.js
@@ -7,7 +7,7 @@ export default async function bookshelf(fastify, opts) {
   fastify.route({
     method: 'POST',
     path: '/bookshelf',
-    handler: onSaveBook,
+    handler: onSaveToBookshelf,
   });
 
   fastify.route({
@@ -16,40 +16,23 @@ export default async function bookshelf(fastify, opts) {
     handler: onUserBookshelf,
   });
 
-  async function onSaveBook(req, reply) {
-    const body = JSON.parse(req.body);
-    const { book } = body;
-    const bookPersisted = await prisma.book.create({
-      data: book,
-    });
-    return bookPersisted;
-  }
-
   async function onSaveToBookshelf(req, reply) {
-    return true;
-    // try {
-    //   const { userId, book } = JSON.parse(
-    //     request.body as string
-    //   ) as SaveToBookshelfBody;
-    //   const data = book;
-    //   data.userIDs = [userId];
-    //   const persistedBook: Prisma.BookCreateInput = await prisma.book.create({
-    //     data,
-    //   });
-    //   //Update the user with the book id associated
-    //   const updateUser = await prisma.user.update({
-    //     where: {
-    //       id: userId,
-    //     },
-    //     data: {
-    //       bookIDs: persistedBook.id,
-    //     },
-    //   });
-    //   return persistedBook;
-    // } catch (error) {
-    //   console.log('##### ERROR ####: ', error);
-    //   throw new Error(error as string);
-    // }
+    try {
+      const { userId, book } = JSON.parse(req.body);
+      const persistedBook = await prisma.book.create({
+        data: {
+          ...book,
+          users: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+      return persistedBook;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async function onUserBookshelf(req, reply) {
